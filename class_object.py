4051489701a86cdf64c836f5e3fbdfc99f20ea5c
shorav24oct <sharmasaurav142@gmail.com>
2026-06-25 05:16:18 +0530

    class & instance variable

--- class_object.py
+++ class_object.py
@@ -1,10 +1,18 @@
 from loguru import logger
 
-class User:
-    def __init__(self, name, age, location=None):
-        self.name = name
-        self.age = age
-        self.location = location
+class Labour:
+    total_labourers = 0 # Class variable to keep track of total labourers
 
-user1 = User("Alice", 30)
-logger.info("User created: {}", user1.__dict__)
+    def __init__(self, first_name, last_name, wage):
+        self.first_name = first_name # Instance variable
+        self.last_name = last_name
+        self.wage = wage
+        Labour.total_labourers += 1
+
+labourer1 = Labour("John", "Doe", 20)
+logger.info("Labourer created: {}", labourer1.__dict__)
+
+labourer2 = Labour("Jane", "Smith", 25)
+logger.info("Labourer created: {}", labourer2.__dict__)
+
+logger.info("Total labourers: {}", Labour.total_labourers)
